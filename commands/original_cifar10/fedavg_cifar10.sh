c39954e71004e3ee2ed2f8b6934b3df3a112dfd3
vaseline555 <sjhahn11512@gmail.com>
2023-07-29 12:49:10 +0900

    adjust commands

--- commands/original_cifar10/fedavg_cifar10.sh
+++ commands/original_cifar10/fedavg_cifar10.sh
@@ -7,7 +7,7 @@ python3 main.py \
     --dataset CIFAR10 \
     --split_type iid --test_size -1 \
     --model_name SimpleCNN --crop 24 --randhf 0.1 --randjit 0.1 --imnorm --hidden_size 64 \
-    --algorithm fedavg --eval_fraction 1 --eval_type local --eval_every 1 --eval_metrics acc1 \
+    --algorithm fedavg --eval_fraction 1 --eval_type local --eval_every 1 --eval_metrics acc1 acc5 \
     --K 100 --R 1000 --E 5 --C 0.1 --B 50 --beta 0 \
     --optimizer SGD --lr 0.15 --lr_decay 0.99 --lr_decay_step 1 --criterion CrossEntropyLoss
 
@@ -17,6 +17,6 @@ python3 main.py \
     --dataset CIFAR10 \
     --split_type patho --test_size -1 \
     --model_name SimpleCNN --crop 24 --randhf 0.1 --randjit 0.1 --imnorm --hidden_size 64 \
-    --algorithm fedavg --eval_type local --eval_every 1 --eval_metrics acc1 \
+    --algorithm fedavg --eval_type local --eval_every 1 --eval_metrics acc1 acc5 \
     --K 100 --R 1000 --E 5 --C 0.1 --B 50 --beta 0 \
     --optimizer SGD --lr 0.01 --lr_decay 0.99 --lr_decay_step 1 --criterion CrossEntropyLoss
